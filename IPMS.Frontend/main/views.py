b0204039b8ed2c1adf469c424a8478922cc348cc
charlie <charliemurray2@gmail.com>
2025-06-07 23:28:31 +0100

    Adding expected return into the mixer

--- IPMS.Frontend/main/views.py
+++ IPMS.Frontend/main/views.py
@@ -11,17 +11,13 @@ def get_minimum_variance(request):
     if request.method == "POST":
         try:
             asset_info = json.loads(request.POST.get("asset_info", "[]"))
-            covariance = json.loads(request.POST.get("covariance", "[]"))
+            covariance = json.loads(request.POST.get("correlation", "[]"))
+            expected_return = json.loads(request.POST.get("expected_return", "[]"))
 
             json_payload = {
                 "AssetInformation": asset_info,
-                "Covariance": covariance
-            }
-
-            json_payload_line = {
-                "AssetInformation": asset_info,
-                "Covariance": covariance,
-                "ExpectedReturn": 0.08
+                "Correlation": covariance,
+                "ExpectedReturn": expected_return
             }
 
             response = requests.post(
@@ -29,24 +25,16 @@ def get_minimum_variance(request):
                 json=json_payload
             )
 
-            response_line = requests.post(
-                "http://127.0.0.1:5000/minimum-variance-line",
-                json=json_payload_line
-            )
-
             response.raise_for_status()
             data = response.json()
 
-            response_line.raise_for_status()
-            data_line = response_line.json()
-
         except (ValueError, json.JSONDecodeError) as parse_error:
             data = {"error": f"Invalid input format: {parse_error}"}
-            data_line = {"error": f"Invalid input format: {parse_error}"}
         except requests.RequestException as api_error:
             data = {"error": str(api_error)}
-            data_line = {"error": str(api_error)}
 
-        return render(request, 'index.html', {'api_data': data, 'graph_point': data, "graph_line": data_line})
+        return render(request, 'index.html', {
+            'api_data': data["Minimum Portfolio"], 'graph_point': data["Minimum Portfolio"], "graph_line": data['Minimum Frontier']
+        })
 
     return render(request, 'index.html', {'api_data': "Invalid request", 'graph_point': None})
